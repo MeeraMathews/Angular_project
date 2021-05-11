1678cf943c36ac21470c0ec43973da241fc36004
Meera Mathews <meeramathewsk@gmail.com>
2021-05-11 21:51:51 +0530

    Add angular-project/

---
+++ angular-project/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  
+  clicked=false;
+  
+
+    Months = [
+    { 
+      id: 1,
+      name: 'Month '
+    },
+    {
+      id: 2, 
+      name: 'Month '
+    },
+    {
+      id: 3,
+      name: 'Month '
+    },
+    {
+      id: 4, 
+      name: 'Month ' 
+    },
+    {
+      id: 5, 
+      name: 'Month ' 
+    },
+  ];
+  
+  
+
+}
